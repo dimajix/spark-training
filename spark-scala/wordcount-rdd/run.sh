122027b5f2aa4bbc219f0d24f0241176c0802f70
Kaya Kupferschmidt <k.kupferschmidt@dimajix.de>
2018-07-04 17:33:02 +0200

    Minor fixes and updates

--- spark-scala/wordcount-rdd/run.sh
+++ spark-scala/wordcount-rdd/run.sh
@@ -2,7 +2,7 @@
 basedir=$(readlink -f $(dirname $0))
 
 APP_NAME="spark-wordcount-rdd"
-APP_MAIN="de.dimajix.training.spark.wordcount.WordCountDriver"
+APP_MAIN="de.dimajix.training.spark.wordcount.Driver"
 APP_VERSION="1.0.0"
 
 source $basedir/conf/common.sh
