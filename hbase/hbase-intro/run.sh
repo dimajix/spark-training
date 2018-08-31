32b3dfb24bf6a3e70892e1daca2d4cfc5f48b265
Kaya Kupferschmidt <k.kupferschmidt@dimajix.de>
2018-08-31 06:35:45 +0200

    Removed some old examples

--- hbase/hbase-intro/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-APP_NAME="hbase-intro"
-APP_MAIN="de.dimajix.training.hbase.Example"
-APP_VERSION="1.0.0"
-
-JAR_NAME="target/$APP_NAME-$APP_VERSION-jar-with-dependencies.jar"
-
-hadoop jar $JAR_NAME $APP_MAIN $@
