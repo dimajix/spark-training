f02270f6741942e47f4394fb87375f09de0658c2
Kaya Kupferschmidt <k.kupferschmidt@dimajix.de>
2020-05-11 11:31:36 +0200

    Remove old RDD streaming examples

--- hadoop/skeleton/src/main/java/de/dimajix/training/hadoop/skeleton/SkeletonMapper.java
+++ hadoop/skeleton/src/main/java/de/dimajix/training/hadoop/skeleton/SkeletonMapper.java
@@ -1,16 +1,10 @@
 package de.dimajix.training.hadoop.skeleton;
 
-import org.apache.commons.csv.CSVFormat;
-import org.apache.commons.csv.CSVParser;
-import org.apache.commons.csv.CSVRecord;
 import org.apache.hadoop.io.FloatWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
 import java.util.HashMap;
 
